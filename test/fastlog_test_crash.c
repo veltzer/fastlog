3c541691ad6cc0966c7d86969efb39b47784fa53
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-09 17:10:19 +0200

    more stuff

--- test/fastlog_test_crash.c
+++ test/fastlog_test_crash.c
@@ -1,4 +1,9 @@
-#include <fastlog.h>
+#include <fastlog.h> // for the fastlog_* functions
+#include <sys/types.h> // for kill(2)
+#include <signal.h> // for kill(2)
+#include <unistd.h> // for pause(2)
+#include <sys/time.h> // for getrlimit(2), setrlimit(2)
+#include <sys/resource.h> // for getrlimit(2), setrlimit(2)
 
 /*
  * This is an application that test what happens when fastlog crashes.
@@ -6,6 +11,22 @@
  */
 
 int main(int argc,char** argv,char ** envp) {
+	// lets check that we can dump core
+	struct rlimit limit;
+	int ret=getrlimit(RLIMIT_CORE,&limit);
+	if(ret==-1) {
+		perror("getrlimit");
+		return -1;
+	}
+	printf("core size is cur=%ld, max=%ld\n",limit.rlim_cur,limit.rlim_max);
+	// lets set the limit to some good value
+	limit.rlim_cur=50000;
+	ret=setrlimit(RLIMIT_CORE,&limit);
+	if(ret==-1) {
+		perror("setrlimit");
+		return -1;
+	}
+	printf("set rlimit for core\n");
 	fastlog_config conf;
 	fastlog_config_init(&conf);
 	fastlog_init(&conf);
@@ -13,5 +34,13 @@ int main(int argc,char** argv,char ** envp) {
 		fastlog_log(&conf,"did you know that %d+%d=%d",i,i+1,i+i+1);
 	}
 	fastlog_close(&conf);
+	// lets kill ourselves
+	ret=kill(0,SIGSEGV);
+	if(ret==-1) {
+		perror("kill");
+		return -1;
+	}
+	// lets wait for the signal to arrive
+	pause();
 	return 0;
 }
