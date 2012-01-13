3ca579a4ab00379347574e4aaefe2818ef0a38e9
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-14 00:51:24 +0200

    better installation

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -3,4 +3,4 @@
 # this scrip will install all the required packages that you need on
 # ubuntu to compile and work with this package.
 
-sudo apt-get install doxygen
+sudo apt-get install doxygen libcpufreq-dev
