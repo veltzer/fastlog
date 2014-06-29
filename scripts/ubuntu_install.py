fd3dd7fe727a1dc37c35d5e537d760824ef0461e
Mark Veltzer <mark.veltzer@gmail.com>
2014-06-29 15:17:12 +0300

    better install script

---
+++ scripts/ubuntu_install.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# this script will install all the required packages that you need on
+# ubuntu to compile and work with this package.
+
+# TODO:
+
+import subprocess # for check_call()
+
+packs=[
+	'doxygen',
+	'libcpufreq-dev',
+]
+
+args=['sudo','apt-get','install']
+args.extend(packs)
+subprocess.check_call(args)
