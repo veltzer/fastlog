31a7845522cc39e8d533e9aa6ba00aa7f39a05e4
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-25 21:30:11 +0300

    fixing deps

--- config/deps.py
+++ config/deps.py
@@ -1,14 +1,4 @@
-'''
-dependencies for this project
-'''
-
-def populate(d):
-    d.packs=[
-        'doxygen',
-        'libcpufreq-dev',
-    ]
-
-def get_deps():
-    return [
-        __file__, # myself
-    ]
+packages=[
+    'doxygen',
+    'libcpufreq-dev',
+]
